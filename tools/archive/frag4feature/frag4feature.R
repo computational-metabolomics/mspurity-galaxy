9017bf5c5c62c85fd59aefff2a09bb2a79cd343d
tomnl <thomas.nigel.lawson@gmail.com>
2017-10-19 09:53:18 +0100

    update of structure

---
+++ tools/archive/frag4feature/frag4feature.R
@@ -0,0 +1,50 @@
+library(msPurity)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-o", "--out_dir"), type="character"),
+  make_option("--pa", type="character"),
+  make_option("--xset", type="character"),
+  make_option("--ppm", default=10),
+  make_option("--plim", default=0.0),
+  make_option("--convert2RawRT", action="store_true"),
+  make_option("--mostIntense", action="store_true"),
+  make_option("--cores", default=4)
+)
+
+# store options
+opt<- parse_args(OptionParser(option_list=option_list))
+
+loadRData <- function(rdata_path, name){
+#loads an RData file, and returns the named xset object if it is there
+    load(rdata_path)
+    return(get(ls()[ls() == name]))
+}
+
+# Requires
+pa <- loadRData(opt$pa, 'pa')
+xset <- loadRData(opt$xset, 'xset')
+
+pa@cores <- opt$cores
+
+if(is.null(opt$mostIntense)){
+    mostIntense = FALSE
+}else{
+    mostIntense = TRUE
+}
+
+if(is.null(opt$convert2RawRT)){
+    convert2RawRT = FALSE
+}else{
+    convert2RawRT= TRUE
+}
+
+print(pa@filelist)
+pa <- msPurity::frag4feature(pa=pa, xset=xset, ppm=opt$ppm, plim=opt$plim,
+                            intense=opt$mostIntense, convert2RawRT=convert2RawRT)
+
+save(pa, file=file.path(opt$out_dir, 'frag4feature.RData'))
+
+print(head(pa@puritydf))
+
+write.table(pa@grped_df, file.path(opt$out_dir, 'frag4feature.tsv'), row.names=FALSE, sep='\t')
---
+++ tools/archive/track_rt_raw/track_rt_raw.R
@@ -0,0 +1,22 @@
+library(xcms)
+library(optparse)
+
+option_list <- list(
+  make_option(c("--xset_path"), type="character"),
+  make_option(c("-o", "--out_dir"), type="character")
+)
+
+# store options
+opt<- parse_args(OptionParser(option_list=option_list))
+
+load(opt$xset_path)
+
+print(xset)
+
+rtraw <- xset@peaks[,c('rt', 'rtmin', 'rtmax')]
+colnames(rtraw) <- c('rt_raw','rtmin_raw','rtmax_raw')
+xset@peaks <- cbind(xset@peaks, rtraw)
+
+
+print('saving RData')
+save.image(file.path(opt$out_dir, 'xset_rt_raw_tracked.RData'))
