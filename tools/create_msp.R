b168ab505956e6adc0f70bc69965b62fbb00b56a
tomnl <thomas.nigel.lawson@gmail.com>
2018-12-05 11:54:56 +0000

    adding tests, updating for metadata

--- tools/create_msp.R
+++ tools/create_msp.R
@@ -9,19 +9,41 @@ option_list <- list(
   make_option(c("-meta","--metadata"),type="character"),
   make_option(c("-metac","--metadata_cols"),type="character"),
   make_option(c("-a","--adduct_split"),type="character"),
-  make_option(c("-x","--xcms_groupids"),type="character")
+  make_option(c("-x","--xcms_groupids"),type="character"),
+  make_option(c("-o","--out_dir"),type="character", default=".")
 )
 opt <- parse_args(OptionParser(option_list=option_list))
 
 print(opt)
 
-load(opt$purity_dataset)
+load(opt$rdata_input)
 
-metadata <- read.table(opt$metadata,  header = TRUE, sep='\t', stringsAsFactors = FALSE, check.names = FALSE)
+if (is.null(opt$metadata)){
+  metadata <- NULL
+}else{
+  metadata <- read.table(opt$metadata,  header = TRUE, sep='\t', stringsAsFactors = FALSE, check.names = FALSE) 
+  print(head(metadata))
+}
+
+if(is.null(opt$adduct_split)){
+  adduct_split <- FALSE
+}else{
+  adduct_split <- TRUE
+}
+
+if (is.null(opt$xcms_groupids)){
+  xcms_groupids <- NULL
+}else{
+  xcms_groupids <- trimws(strsplit(opt$xcms_groupids, ',')[[1]])  
+}
+
 
 msPurity::createMSP(pa, 
-                    msp_file_pth = 'lcmsms.msp',
+                    msp_file_pth = file.path(opt$out_dir, 'lcmsms_spectra.msp'),
                     metadata = metadata,
                     metadata_cols = opt$metadata_cols,
                     method = opt$method, 
-                    adduct_split = opt$adduct_split)
+                    adduct_split = adduct_split,
+                    xcms_groupids = xcms_groupids)
+
+print('msp created')
